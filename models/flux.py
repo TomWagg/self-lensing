bfb24887ed438c7479ec42a812406066c466ef75
Miguel <mtvaranda@gmail.com>
2022-11-09 19:40:08 +0000

    added clone branch

--- models/flux.py
+++ models/flux.py
@@ -1,6 +1,7 @@
 import numpy as np
 import astropy.units as u
 import astropy.constants as const
+import matplotlib.pyplot as plt
 
 
 """
@@ -14,20 +15,41 @@ import astropy.constants as const
 """
 
 
-def ellipsoidal_variation():
+def ellipsoidal_variation(phase, M_bh, M_star, inc, R_star, period):
+    # time =[1, 2 , 10] * u.day
+    # phase = [0, pi, 2pi]
+    
+    sep = keplers_third_law(M_bh=M_bh, M_star=M_star, period=period)
+    
+    ev = -np.cos(phase * 2)
+    amplitude = M_bh * np.sin(inc) / M_star * (R_star / sep)**3 * np.sin(inc)
+    return amplitude.decompose() * ev
     # TODO
-    raise NotImplementedError
 
 
-def doppler_beaming():
-    # TODO
-    raise NotImplementedError
+def keplers_third_law(period, M_bh, M_star):
+    sep = (const.G * (M_star + M_bh)*period**2 / (4 * np.pi**2))**(1/3)
+    return sep.to(u.AU)
+
+def doppler_beaming(phase, period, inc, M_bh, M_star):
+    amplitude = 2.8e-3 * np.sin(inc) * (period / (1 * u.day))**(-1/3)*((M_bh + M_star) / u.solMass)**(-2/3) * (M_bh / u.solMass) 
+    db = np.sin(phase)
+    return amplitude.decompose() * db
 
 
-def self_lensing():
+def self_lensing(phase, M_bh, R_star, ):
     # TODO
     raise NotImplementedError
 
 
 def relative_flux():
     return ellipsoidal_variation() + doppler_beaming() + self_lensing()
+
+
+print(ellipsoidal_variation(phase=0, M_bh=9 * u.Msun, M_star=600 * u.Msun, inc=np.pi/2, R_star=20 * u.Rsun, sep=6 * u.AU))
+
+x_vals = np.linspace(0, 2 * np.pi, 1000)
+y_vals = ellipsoidal_variation(phase=x_vals, M_bh=9 * u.Msun, M_star=600 * u.Msun, inc=np.pi/2, R_star=20 * u.Rsun, sep=6 * u.AU)
+
+plt.plot(x_vals, y_vals)
+plt.show()
